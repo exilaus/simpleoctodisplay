f9e52eccd2528da2a63887acfb7b4c982ba29689
Mauro <exilaus@hotmail.com>
2014-03-01 09:08:37 +0100

    Update display.sh

--- display.sh
+++ display.sh
@@ -1,7 +1,7 @@
 curl -s "http://localhost/api/state?apikey=your_key" > info.txt
 
-STATE=$(cat info.txt| jq '.state.state')
-if [ "$STATE" = "null" ]
+STATE=$(cat info.txt| jq '.state.stateString')
+if [ "$STATE" = "Printing" ]
 then
 ./Displayprn
 else
@@ -9,7 +9,7 @@ HE=$(cat info.txt| jq '.temperature.extruder.current')
 HB=$(cat info.txt| jq '.temperature.bed.current')
 ETA=$(cat info.txt| jq '.progress.printTimeLeft')
 PROG=$(cat info.txt| jq '.progress.progress')
-FILE=$(cat info.txt| jq '.currentZ.job.filename')
+FILE=$(cat info.txt| jq '.job.filename')
 
 ./Displayprn MAKIBOX_Stat: HE:$HE-HB:$HB ETA:$ETA File:$FILE COMPLETE:$PROG LINEFREE_EXI
 
